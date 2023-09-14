42c9d3502f292c64132f42169581a181e63c811b
dren.s <dren.s@gjirafa.com>
2023-09-14 22:24:12 +0200

    async script

--- src/app/page.js
+++ src/app/page.js
@@ -46,8 +46,8 @@ export default function Home() {
         {/* Content of the first div */}
       </div>
       <div className='absolute inset-0 bg-opacity-20 backdrop-blur-sm'></div>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" async></script>
+      <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js" async></script>
     </>
   )
 }
