eb1be29c6e7583246d2a47334b973cd841636abf
dren.s <dren.s@gjirafa.com>
2023-09-15 19:01:27 +0200

    Updated Library tab

--- src/components/Library.js
+++ src/components/Library.js
@@ -16,8 +16,16 @@ const bebas_neue = Bebas_Neue({
 export default function Library() {
     return (
         <>
-            <div className='flex flex-col gap-4 justify-center items-center'>
-                
+            <div className='flex flex-col justify-center items-center py-20 text-center gap-6'>
+                <h1 className='text-2xl md:text-3xl text-gray-300'>You are not signed in.</h1>
+                <p className='text-lg md:text-xl text-gray-300'>Please sign in to see your monthly and ai generated Spotify playlists from Spoti-Engine</p>
+                <button
+                    type="button"
+                    className="inline-block rounded border-2 border-neutral-50 px-6 py-2 text-xs font-bold uppercase leading-normal text-gray-300 transition duration-150 ease-in-out hover:bg-gray-300 hover:text-black"
+                    data-te-ripple-init>
+                    Sign In
+                </button>
+
             </div>
         </>
     )
