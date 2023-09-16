3151f8a690654c55c18f06fdd8c139fbd01cc157
dren.s <dren.s@gjirafa.com>
2023-09-16 17:00:37 +0200

    v

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,8 +20,8 @@ export default function Document() {
         <Main />
         <NextScript />
 
-        <script src="three.r134.min.js"></script>
-        <script src="vanta.birds.min.js"></script>
+        <script src="three.r134.min.js" async></script>
+        <script src="vanta.birds.min.js" async></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" async></script>
         <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js" async></script>
 
