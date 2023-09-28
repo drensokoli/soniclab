e3cae4c2fcb838b3372f9bab46e85076fabb0f9d
Dren Sokoli <dren.s@gjirafa.com>
2023-09-28 14:04:56 +0200

    returned app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,21 @@ export default function App({ Component, pageProps, router }: AppProps) {
 }
 
 function WrappedApp({ Component, pageProps, router }: WrappedAppProps) {
-  
+  const { data: session } = useSession();
+  const getUser = async () => {
+    const response = await fetch('/api/getUser', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userEmail: session?.user?.email }),
+    });
+    const user = await response.json();
+  };
+
+  useEffect(() => {
+    if (session)
+    getUser();
+  }, [session, router.pathname]);
+
   return (
     <div>
       <Component {...pageProps} />
