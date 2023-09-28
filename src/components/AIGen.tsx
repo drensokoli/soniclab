ca4e0c47cda09e43acf05a708651e68b62d4fe44
Dren Sokoli <dren.s@gjirafa.com>
2023-09-28 15:04:01 +0200

    client side error test fix 2

--- src/components/AIGen.tsx
+++ src/components/AIGen.tsx
@@ -78,45 +78,47 @@ export default function AIGen({ spotifyClientId, spotifyClientSecret }: { spotif
 
     return (
         <>
-                {infoBubble && (
-                    <div className='h-[80px] w-[250px] fixed top-12 right-12  flex flex-col justify-center items-center  bg-[#f33f81] rounded-xl text-gray-200'>
-                        {playlistId ? (
-                            <a href={`https://open.spotify.com/playlist/${playlistId}?si=d0a3c3daf8ce4079`} target='_blank'>
-                                <div className='text-center z-20 flex flex-col gap-2 justify-center items-center'>
-                                    <h1>{playlistName}</h1>
-                                    <SiSpotify className='w-5 h-5' />
-                                </div>
-                            </a>
-                        ) : (
-                            <h1>Playlist failed to be created</h1>
-                        )}
-                    </div>
-                )}
             {
                 session ? (
                     <>
                         {!(songIds.length > 0) ? (
-                            <div className='flex flex-col justify-center items-center' id='song-generator'>
-                                <h1 className='text-xl md:text-2xl py-4 font-bold text-gray-300 text-center'>Write your playlist description here</h1>
-                                <textarea id="description" rows={4} className="block p-2.5 w-full md:w-3/4 text-md text-gray-300 bg-transparent rounded-lg border border-gray-200" placeholder="The playlist should contain HipHop and R&B songs from the 90s..."></textarea>
-                                <div className='flex justify-center items-center w-full md:w-3/4 py-4'>
-                                    <div className='flex flex-row items-center cursor-pointer text-gray-300 hover:text-gray-400 w-fit'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                                        </svg>
-                                        <h1 className='text-sm'>Get inspired</h1>
+                            <>
+                                {infoBubble && (
+                                    <div className='h-[80px] w-[250px] fixed top-12 right-12  flex flex-col justify-center items-center  bg-[#f33f81] rounded-xl text-gray-200'>
+                                        {playlistId ? (
+                                            <a href={`https://open.spotify.com/playlist/${playlistId}?si=d0a3c3daf8ce4079`} target='_blank'>
+                                                <div className='text-center z-20 flex flex-col gap-2 justify-center items-center'>
+                                                    <h1>{playlistName}</h1>
+                                                    <SiSpotify className='w-5 h-5' />
+                                                </div>
+                                            </a>
+                                        ) : (
+                                            <h1>Playlist failed to be created</h1>
+                                        )}
                                     </div>
-                                </div>
+                                )}
+                                <div className='flex flex-col justify-center items-center' id='song-generator'>
+                                    <h1 className='text-xl md:text-2xl py-4 font-bold text-gray-300 text-center'>Write your playlist description here</h1>
+                                    <textarea id="description" rows={4} className="block p-2.5 w-full md:w-3/4 text-md text-gray-300 bg-transparent rounded-lg border border-gray-200" placeholder="The playlist should contain HipHop and R&B songs from the 90s..."></textarea>
+                                    <div className='flex justify-center items-center w-full md:w-3/4 py-4'>
+                                        <div className='flex flex-row items-center cursor-pointer text-gray-300 hover:text-gray-400 w-fit'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                            </svg>
+                                            <h1 className='text-sm'>Get inspired</h1>
+                                        </div>
+                                    </div>
 
-                                <button
-                                    type="button"
-                                    className="inline-block rounded border-2 border-[#f33f81] px-6 py-2 text-xs font-bold uppercase leading-normal text-gray-300 transition duration-150 ease-in-out hover:bg-[#f33f81] hover:text-black"
-                                    data-te-ripple-init
-                                    onClick={() => fetchSongIds()}
-                                >
-                                    Generate Songs
-                                </button>
-                            </div>
+                                    <button
+                                        type="button"
+                                        className="inline-block rounded border-2 border-[#f33f81] px-6 py-2 text-xs font-bold uppercase leading-normal text-gray-300 transition duration-150 ease-in-out hover:bg-[#f33f81] hover:text-black"
+                                        data-te-ripple-init
+                                        onClick={() => fetchSongIds()}
+                                    >
+                                        Generate Songs
+                                    </button>
+                                </div>
+                            </>
                         ) : (
                             <div className='flex flex-col justify-center items-center gap-4'>
                                 <h1 className='text-xl py-4 md:text-2xl font-bold text-gray-300 text-center'>Pick a name for your playlist</h1>
