f7256d5dde55e15e0a8dbfa6826fe6f373203451
Dren Sokoli <dren.s@gjirafa.com>
2023-09-28 13:59:07 +0200

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,10 @@ export default function Document() {
 
       <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" defer></script>
       <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js" defer></script>
-      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
+
+      <div className='hidden'>
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
+      </div>
 
       <Head />
       <body>
