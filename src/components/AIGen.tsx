562bd435e6e7e2fa9fa38890a1978a2cf01c05e2
Dren Sokoli <dren.s@gjirafa.com>
2023-10-02 10:18:02 +0200

    added spotify icon to playlist link

--- src/components/AIGen.tsx
+++ src/components/AIGen.tsx
@@ -153,8 +153,8 @@ export default function AIGen({
                                 <div className='flex flex-col justify-center items-center py-4'>
                                     <a href={`https://open.spotify.com/playlist/${playlistId}`} target='_blank'>
                                         <div className='flex flex-col gap-2 justify-center items-center border-2 border-white px-12 py-4 rounded-xl'>
-                                            {/* <SiSpotify className='text-gray-300' /> */}
-                                             <h1 className='text-gray-300'>Open in Spotify</h1>
+                                            <SiSpotify className='text-gray-300' />
+                                            <h1 className='text-gray-300'>Open in Spotify</h1>
                                         </div>
                                     </a>
                                 </div>
