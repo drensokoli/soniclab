2534e2b80709a7f54f612def96db99b1a55c7e7e
Dren Sokoli <dren.s@gjirafa.com>
2023-10-02 10:01:35 +0200

    Mos boft qetas s'di mo

--- src/components/AIGen.tsx
+++ src/components/AIGen.tsx
@@ -150,9 +150,11 @@ export default function AIGen({
                     ) : !(songIds.length > 0) ? (
                         <>
                             {playlistId && (
-                            <div>
-                                {playlistId}
-                            </div>
+                                <div>
+                                    <a href={`https://open.spotify.com/playlist/${playlistId}?si=d0a3c3daf8ce4079`} target='_blank'>
+                                        Open in Spotify
+                                    </a>
+                                </div>
                             )}
                             <AIGenForm
                                 setDescription={setDescription}
