054e885a59fda0fc2a1830098d2dd8593e73a223
dren.s <dren.s@gjirafa.com>
2023-10-15 21:10:47 +0200

    ad block recovery

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react';
 
 export default function Document() {
+  useEffect(() => {
+    function signalGooglefcPresent() {
+      if (!(window.frames as any)['googlefcPresent']) {
+        if (document.body) {
+          const iframe = document.createElement('iframe');
+          iframe.setAttribute('style', 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;');
+          iframe.style.display = 'none';
+          iframe.name = 'googlefcPresent';
+          document.body.appendChild(iframe);
+        } else {
+          setTimeout(signalGooglefcPresent, 0);
+        }
+      }
+    }
+    signalGooglefcPresent();
+  }, []);
+
   return (
     <Html lang="en">
       <title>SpotiLab</title>
@@ -16,8 +34,9 @@ export default function Document() {
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
 
       <Head>
-      <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
+        <script async src="https://fundingchoicesmessages.google.com/i/pub-3464540666338005?ers=1" nonce="RH0oI1spY4eKumxENDXqgg"></script>
+        <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
       </Head>
       <body>
         <Main />
