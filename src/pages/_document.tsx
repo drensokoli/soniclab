add01969f1a378f954dc36a5385285829fdb3b74
dren.s <dren.s@gjirafa.com>
2023-10-15 18:56:47 +0200

    ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <title>SpotiLab</title>
       <meta name="SpotiLab" content="" />
+      <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
 
       <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" defer></script>
       <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js" defer></script>
@@ -12,12 +13,16 @@ export default function Document() {
       <div className='hidden'>
         <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="drensokoli" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18" defer></script>
       </div>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
 
-      <Head />
+      <Head>
+      <meta name="google-adsense-account" content="ca-pub-3464540666338005" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
-
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3464540666338005" crossOrigin="anonymous"></script>
       </body>
     </Html>
   )
