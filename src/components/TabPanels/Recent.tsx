e5013b2cd797794b4794fa95de679f85b69d35f5
dren.s <dren.s@gjirafa.com>
2024-01-04 14:28:44 +0100

    Error?

--- src/components/TabPanels/Recent.tsx
+++ src/components/TabPanels/Recent.tsx
@@ -175,17 +175,6 @@ export default function Recent({
 
                 </div>
             </div>
-            <div className="flex flex-col justify-center items-center text-gray-300 py-1">
-                <div className="flex flex-row justify-center items-center gap-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                    </svg>
-                    <h1 className='text-sm'>We are not related to Spotify AB or any of it's partners in any way.</h1>
-                </div>
-                <h1
-                    // className="text-sm"
-                >All images are copyrighted by their respective copyright owners.</h1>
-            </div>
             <div className="flex flex-row justify-center">
                 <div className="flex items-center justify-end w-full py-4">
                     <View setView={setView} />
