91f53c9374baf921e8c6a561551a5f9190cf5b86
dren.s <dren.s@gjirafa.com>
2024-01-05 20:07:04 +0100

    minor updates

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,6 +4,10 @@ export default function Footer() {
   return (
 
     <footer className="p-4 bottom-0 w-full flex flex-col items-center justify-center z-50">
+
+      <div className="flex flex-col justify-center items-center text-gray-300 py-1">
+        <h1 className='text-center text-sm text-gray-400'>We are not related to Spotify AB or any of its partners in any way. All images are copyrighted by their respective copyright owners.</h1>
+      </div>
       <h1 className='text-gray-400 text-center'>Made with ❤️ by
         <span className="text-blue-300">
           <a href="https://www.linkedin.com/in/dren-sokoli-0003a81a1/" target="_blank"> Dren Sokoli</a>
