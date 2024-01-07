125fba0b15caad6bab9aff0e3d96820b6f280fd5
dren.s <dren.s@gjirafa.com>
2024-01-07 15:08:06 +0100

    minor fix

--- src/components/Helpers/RecommendSongs.tsx
+++ src/components/Helpers/RecommendSongs.tsx
@@ -9,8 +9,7 @@ const jakarta = Plus_Jakarta_Sans({
 export default function RecommendSongs({fetchRecommendation}: {fetchRecommendation: any}) {
 
     return (
-        <button
-            type="button"
+        <div
             className="flex flex-col items-center justify-center gap-2 p-3 border-2 border-[#f33f81] border-opacity-60 backdrop-blur text-gray-200 rounded-md shadow-md dark:bg-gray-800"
             onClick={() => fetchRecommendation()}
         >            <img src="./plus.png" alt="Add more songs" width={40} height={40} className="mt-4" />
@@ -18,6 +17,6 @@ export default function RecommendSongs({fetchRecommendation}: {fetchRecommendati
                 <img src="./spotify.png" alt="Spotify Icon" width={15} height={15} className="inline-block" />
                 <h1 className={`${jakarta.className} text-xs py-1`}>RECOMMEND MORE SONGS</h1>
             </div>
-        </button>
+        </div>
     )
 }
