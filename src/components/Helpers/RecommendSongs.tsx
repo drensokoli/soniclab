8f8bd17f12abad93e0c08b2e94a400b27dbe8852
dren.s <dren.s@gjirafa.com>
2024-01-07 20:42:40 +0100

    recommend button cursor pointer

--- src/components/Helpers/RecommendSongs.tsx
+++ src/components/Helpers/RecommendSongs.tsx
@@ -10,7 +10,7 @@ export default function RecommendSongs({fetchRecommendation}: {fetchRecommendati
 
     return (
         <div
-            className="flex flex-col items-center justify-center gap-2 p-3 border-2 border-[#f33f81] border-opacity-60 backdrop-blur text-gray-200 rounded-md shadow-md dark:bg-gray-800"
+            className="flex flex-col items-center justify-center gap-2 p-3 cursor-pointer border-2 border-[#f33f81] border-opacity-60 backdrop-blur text-gray-200 rounded-md shadow-md dark:bg-gray-800"
             onClick={() => fetchRecommendation()}
         >            <img src="./plus.png" alt="Add more songs" width={40} height={40} className="mt-4" />
             <div className="flex flex-row justify-center items-center gap-1">
