30b95ca6b3fb188524f8365a9554892cb4064ad2
drensokoli <79976300+drensokoli@users.noreply.github.com>
2024-01-18 13:12:33 +0100

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -18,7 +18,7 @@ export const authOptions = {
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization: "https://accounts.spotify.com/authorize?scope=ugc-image-upload,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-read-private,playlist-read-collaborative,playlist-modify-private,playlist-modify-public,user-follow-modify,user-follow-read,user-top-read,user-read-recently-played,user-library-modify,user-library-read,user-read-email,user-read-private"
+      authorization: "https://accounts.spotify.com/authorize?scope=ugc-image-upload,user-read-currently-playing,playlist-read-private,playlist-read-collaborative,playlist-modify-private,playlist-modify-public,user-follow-read,user-top-read,user-read-recently-played,user-library-read,user-read-email,user-read-private"
     }),
   ],
   secret: process.env.JWT_SECRET,
