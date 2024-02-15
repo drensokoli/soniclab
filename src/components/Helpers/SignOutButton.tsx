cc2fe2599bcaf56a4396ef3eaf4a068b52045694
Dren Sokoli <dren.s@gjirafa.com>
2024-02-15 12:07:56 +0100

    updated pwa manifest bg color

--- src/components/Helpers/SignOutButton.tsx
+++ src/components/Helpers/SignOutButton.tsx
@@ -1,37 +1,20 @@
 import { signOut } from "next-auth/react";
 
 export default function SignOutButton() {
-    const handleSignOut = () => {
-        // Clear localStorage
-        localStorage.clear();
-
-        // Clear sessionStorage
-        sessionStorage.clear();
-
-        // Clear cookies
-        const cookies = document.cookie.split(";");
-        for (let i =  0; i < cookies.length; i++) {
-            const cookie = cookies[i];
-            const eqPos = cookie.indexOf("=");
-            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = name + "=;expires=Thu,  01 Jan  1970  00:00:00 GMT";
-        }
-
-        // Sign out and redirect
-        signOut({ redirect: false });
-        window.location.href = '/'; // Replace '/' with the path you want to redirect to
-    };
-
     return (
         <div className='flex flex-row justify-center items-center pb-4'>
+
             <button
                 type="button"
                 className="inline-block rounded border-2 border-[#f33f81] px-6 py-2 text-xs font-bold uppercase leading-normal text-gray-300 transition duration-150 ease-in-out hover:bg-[#f33f81] hover:text-black z-10"
                 data-te-ripple-init
-                onClick={handleSignOut}
+                onClick={() => {
+                    signOut();
+                    sessionStorage.clear();
+                }}
             >
                 Sign Out
             </button>
         </div>
-    );
+    )
 }
