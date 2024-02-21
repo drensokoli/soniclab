b638a1c1dcb1f747b4d1050aac6ead71616237de
Dren Sokoli <dren.s@gjirafa.com>
2024-02-21 14:58:42 +0100

    updated select top time range

--- src/components/TabPanels/Top.tsx
+++ src/components/TabPanels/Top.tsx
@@ -228,7 +228,7 @@ export default function Top({
                 <div className="flex flex-row items-start gap-2">
                     <View setView={setView} />
                     <select
-                        className="bg-gray-200 mt-1 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-200 mt-1 border h-[38px] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         value={timeRange}
                         onChange={(e) => {
                             setTimeRange(e.target.value);
